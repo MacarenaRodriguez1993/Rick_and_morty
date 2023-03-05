2bdcd16c10b1f3e865dc30d4d6b36a1c5bfacf01
Macarena Rodriguez <maca.b.rodriguez@gmail.com>
2023-03-05 13:10:24 -0300

    se creo service para episodios y se renderizo pagina episodios

--- src/app/pages/episodes/episodes.component.ts
+++ src/app/pages/episodes/episodes.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Episode } from 'src/app/models/episode';
+import { EpisodesService } from 'src/app/service/episodes.service';
 
 @Component({
   selector: 'app-episodes',
   templateUrl: './episodes.component.html',
-  styleUrls: ['./episodes.component.scss']
+  styleUrls: ['./episodes.component.scss'],
 })
 export class EpisodesComponent implements OnInit {
-
-  constructor() { }
-
+  constructor(
+    private episodeService: EpisodesService,
+    private route: ActivatedRoute
+  ) {}
+  episode: Episode | undefined;
   ngOnInit(): void {
+    this.route.params.subscribe((e) => {
+      const id = e['id'];
+      this.episodeService.getEpisodeById(id).subscribe((res) => {
+        this.episode = res;
+      });
+    });
   }
-
 }
--- src/app/service/episodes.service.ts
+++ src/app/service/episodes.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Episode } from '../models/episode';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class EpisodesService {
+  constructor(private http: HttpClient) {}
+  private url_api: string = 'https://rickandmortyapi.com/api/episode';
 
-  constructor() { }
+  // Llamado a la API para traer detalles de un episodio por id
+  getEpisodeById(id: number): Observable<Episode> {
+    return this.http.get<Episode>(`${this.url_api}/${id}`);
+  }
 }
