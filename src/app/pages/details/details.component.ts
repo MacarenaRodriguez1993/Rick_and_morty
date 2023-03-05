56ed82d692bc30b94674d7378850f8130531dc7d
Macarena Rodriguez <maca.b.rodriguez@gmail.com>
2023-03-05 12:57:54 -0300

    render pagina details

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Character } from 'src/app/models/character';
+import { CharactersService } from 'src/app/service/characters.service';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
-  styleUrls: ['./details.component.scss']
+  styleUrls: ['./details.component.scss'],
 })
 export class DetailsComponent implements OnInit {
+  constructor(
+    private http: CharactersService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
-  constructor() { }
+  character: Character | undefined;
+  numberEpisode: string[] = [];
 
   ngOnInit(): void {
+    //Busco detalles de un personaje en particular
+    this.route.params.subscribe((p) => {
+      const id = p['id'];
+      this.http.getCharacterById(id).subscribe((res) => {
+        this.character = res;
+        this.searchEpisodeNumber();
+      });
+    });
   }
 
+  //Metodo para obtener numero del episodio y redireccionar a la pagina con su detalle
+  searchEpisodeNumber() {
+    const search = 'episode/';
+    console.log(this.character);
+    this.character?.episode.forEach((element) => {
+      const i = element.indexOf(search);
+      if (i !== -1) {
+        this.numberEpisode.push(element.substring(i + search.length));
+      }
+    });
+  }
+
+  //redirect a episodio por id
+  goEpisode(e: string) {
+    this.router.navigate([`/episode/${e}`]);
+  }
 }
